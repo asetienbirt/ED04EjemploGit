e4fc3b2a9d56bdab9857b336ab01031bb4246c26
Aitor <asetien@birt.eus>
2026-05-18 10:13:19 +0200

    Version 0. Primer commit del proyecto

---
+++ src/main/java/com/mycompany/ud04ejemplogit/UD04EjemploGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ud04ejemplogit;
+
+/**
+ *
+ * @author Aitor
+ */
+public class UD04EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
